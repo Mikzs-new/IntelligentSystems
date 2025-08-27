df79698f94a21a0c5b0a3215e961767571c18341
Mikzs-new <cayundamichael@gmail.com>
2025-08-27 14:31:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,21 @@ class RuleBasedSystem:
             new_fact_found = False
             for ante, cons in self.rules:
                 if isinstance(ante, list):
-                    condition_set = all(condition in self.facts for condition in ante)
+                    condition_set = True
+                    for condition in ante:
+                        if isinstance(condition, list):
+                            if "NOT" in condition:
+                                for cond_not in condition:
+                                    if cond_not in self.facts:
+                                        condition_set = False
+                            if "OR" in condition:
+                                condition_set = False
+                                for cond in condition:
+                                    if cond in self.facts:
+                                        condition_set = True
+                                        break
+                        elif not condition in self.facts:
+                            condition_set = False
                 else:
                     condition_set = ante in self.facts
                 if condition_set and cons not in self.facts:
@@ -31,7 +45,7 @@ if __name__ == "__main__":
     system.add_rule(["is_mammal", "eats_meat"], "is_carnivore")
     system.add_rule(["is_mammal", "has_hooves"], "is_ungulate")
     system.add_rule(["is_carnivore", "has_tawny_color", "has_dark_spots"], "is_cheetah")
-    system.add_rule(["is_carnivore", "has_tawny_color", "has_black_stripes"], "is_tiger")
+    system.add_rule(["is_carnivore", "has_tawny_color", ["NOT", "has_dark_spots"]], "is_tiger")
     system.add_rule(["is_ungulate", "has_long_neck", "has_long_legs"], "is_giraffe")
     system.add_rule(["is_ungulate", "has_based_stripes"], "is_zebra")
     system.add_rule("is_bird", "is_animal")
@@ -41,7 +55,7 @@ if __name__ == "__main__":
     system.add_fact("eats_meat")
     system.add_fact("has_tawny_color")
 #    system.add_fact("has_dark_spots")
-    system.add_fact("has_black_stripes")
+    system.add_fact("has_orange_color")
 
 
     system.forward_chain()
